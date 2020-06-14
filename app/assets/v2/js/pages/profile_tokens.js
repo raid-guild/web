ddd6485a3012b056e99340692bd9872940d66a9a
iamonuwa <matrix4u2002@gmail.com>
2020-06-14 22:31:17 +0100

    Update profile token file

--- app/assets/v2/js/pages/profile_tokens.js
+++ app/assets/v2/js/pages/profile_tokens.js
@@ -9,7 +9,6 @@ $(document).on("click", "#submit_create_token", (event) => {
 });
 
 async function deployToken(tokenName, tokenSymbol, tokenPrice, tokenSupply) {
-  console.log("Deploying token");
   [user] = await web3.eth.getAccounts();
   const factoryAddress = "0x7bE324A085389c82202BEb90D979d097C5b3f2E8";
   const factory = await new web3.eth.Contract(ptoken_abi.abi, factoryAddress);
